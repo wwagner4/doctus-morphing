7f0153f599895d8fcf7125af24580614b64d16cd
wwagner4 <wwagner4@gmail.com>
2016-03-16 08:26:56 +0100

    refactored drawing

--- morphing-core/src/main/scala/net/entelijan/MorphingController.scala
+++ morphing-core/src/main/scala/net/entelijan/MorphingController.scala
@@ -6,15 +6,19 @@ import doctus.core.template._
 import doctus.core.color._
 import scala.annotation.tailrec
 
-case class Trans(startTime: Long, from: DoctusPoint, to: DoctusPoint, duration: Long, disp: Int) {
+case class Trans(startTime: Long, from: DoctusPoint, to: DoctusPoint, duration: Long) {
 
   def terminated(time: Long) = time - startTime > duration
 
 }
 
+case class Model(trans: Trans, disp: Int)
+
 case class MorphingDoctusTemplate(canvas: DoctusCanvas) extends DoctusTemplate {
 
   val random = new java.util.Random
+
+  val disps = Stream.continually(random.nextInt(360))
 
   override val frameRate = Some(20)
 
@@ -22,12 +26,13 @@ case class MorphingDoctusTemplate(canvas: DoctusCanvas) extends DoctusTemplate {
     PixImageFactory.no0460, PixImageFactory.no0560, PixImageFactory.no0660, PixImageFactory.no0760, PixImageFactory.no0860)
 
   var currentImg = 0
-  var transitions = List.empty[Trans]
+  var models = List.empty[Model]
 
-  def createNextTrans: Unit = {
+  def createNextModels: Unit = {
     val time = System.currentTimeMillis()
 
     def createTransitions(img1: List[DoctusPoint], img2: List[DoctusPoint], startTime: Long): List[Trans] = {
+
       def randomOffset: DoctusVector = {
         val xIntOff = random.nextInt(3) - 1
         val yIntOff = random.nextInt(3) - 1
@@ -37,16 +42,22 @@ case class MorphingDoctusTemplate(canvas: DoctusCanvas) extends DoctusTemplate {
       img1.zip(img2) map {
         case (a1, a2) =>
           val roff = randomOffset
-          val duration = 1000 + random.nextInt(1000)
-          Trans(startTime, a1, a2 + roff, duration, random.nextInt(8))
+          val duration = 5000 + random.nextInt(10000)
+          Trans(startTime, a1, a2 + roff, duration)
       }
+    }
+
+    def createModels(transisions: List[Trans]): List[Model] = {
+      transisions.zip(disps).map { case (trans, disp) => Model(trans, disp) }
     }
 
     val i1 = pixImages(currentImg)
     val i2 = pixImages((currentImg + 1) % pixImages.size)
     val p1 = pointImg(i1)
     val p2 = pointImg(i2)
-    transitions = createTransitions(p1, p2, time)
+    val transitions = createTransitions(p1, p2, time)
+
+    models = createModels(transitions)
     currentImg = (currentImg + 1) % pixImages.size
 
   }
@@ -58,7 +69,7 @@ case class MorphingDoctusTemplate(canvas: DoctusCanvas) extends DoctusTemplate {
 
   def pointImg(pi: PixImage): List[DoctusPoint] = {
     val scale = calcScale(canvas.width, canvas.height, pi.width, pi.height)
-    createPoints1(5000, List.empty[DoctusPoint], pi, scale)
+    createPoints(15000, List.empty[DoctusPoint], pi, scale)
   }
 
   def calcScale(canvW: Double, canvH: Double, imgW: Double, imgH: Double): Adjust = {
@@ -69,16 +80,16 @@ case class MorphingDoctusTemplate(canvas: DoctusCanvas) extends DoctusTemplate {
   }
 
   @tailrec
-  private def createPoints1(cnt: Int, points: List[DoctusPoint], img: PixImage, adj: Adjust): List[DoctusPoint] = {
+  private def createPoints(cnt: Int, points: List[DoctusPoint], img: PixImage, adj: Adjust): List[DoctusPoint] = {
     if (cnt == 0) points
     else {
       val x = ran();
       val y = ran();
       if (isPoint(x, y, img)) {
         val p = DoctusPoint(adj.offx + x * adj.scalex, adj.offy + y * adj.scaley)
-        createPoints1(cnt - 1, p :: points, img, adj)
+        createPoints(cnt - 1, p :: points, img, adj)
       } else {
-        createPoints1(cnt, points, img, adj)
+        createPoints(cnt, points, img, adj)
       }
     }
   }
@@ -100,24 +111,15 @@ case class MorphingDoctusTemplate(canvas: DoctusCanvas) extends DoctusTemplate {
       g.fill(DoctusColorWhite, 100)
       g.rect(DoctusPoint(0, 0), w, h)
     }
-    
+
     def createLine(center: DoctusPoint, disp: Int): (DoctusPoint, DoctusPoint) = {
-      val len = 5.0
+      val len = 15.0
       val lenh = len / 2.0
-      disp match {
-        case 0 => (center + DoctusVector(len, 0), center + DoctusVector(-len, 0))
-        case 1 => (center + DoctusVector(0, lenh), center + DoctusVector(0, -lenh))
-        case 2 => (center + DoctusVector(len, len), center + DoctusVector(-len, -len))
-        case 3 => (center + DoctusVector(lenh, len), center + DoctusVector(-lenh, -len))
-        case 4 => (center + DoctusVector(0, len), center + DoctusVector(0, -len))
-        case 5 => (center + DoctusVector(-lenh, len), center + DoctusVector(lenh, -len))
-        case 6 => (center + DoctusVector(-len, len), center + DoctusVector(len, -len))
-        case 7 => (center + DoctusVector(-len, lenh), center + DoctusVector(len, -lenh))
-        case _ => throw new IllegalArgumentException("disp must be 0-7")
-      }
+      val v0 = DoctusVector(0, lenh).rot(disp * math.Pi / 180)
+      (center + v0, center - v0)
     }
 
-    def drawTrans(trans: Trans, time: Long): Unit = {
+    def drawModel(model: Model, time: Long): Unit = {
       def adj(v: Double, max: Double): Double = {
         if (v < 0) {
           (1.0 - math.abs(v % 1.0)) * max
@@ -126,37 +128,32 @@ case class MorphingDoctusTemplate(canvas: DoctusCanvas) extends DoctusTemplate {
         }
       }
 
+      val trans = model.trans
       val t = (time - trans.startTime).toDouble
       val dp = if (t < trans.duration) {
         val x = trans.from.x + (t / trans.duration) * (trans.to.x - trans.from.x)
         val y = trans.from.y + (t / trans.duration) * (trans.to.y - trans.from.y)
         DoctusPoint(adj(x, w), adj(y, h))
       } else {
-        val x = trans.to.x
-        val y = trans.to.y
+        val x = model.trans.to.x
+        val y = model.trans.to.y
         DoctusPoint(adj(x, w), adj(y, h))
       }
       val center = DoctusPoint(dp.x, dp.y)
-      val (from, to) = createLine(center, trans.disp)
+      val (from, to) = createLine(center, model.disp)
       g.line(from, to)
     }
 
-    def currentTime: Long = System.currentTimeMillis()
-
-    val time = currentTime
-    if (transitions.isEmpty) {
-      createNextTrans
-    }
+    if (models.isEmpty) createNextModels
 
     drawBackground(g)
-    
+
     //g.fill(DoctusColorBlack, 10)
     //g.noStroke()
-    
     g.stroke(DoctusColorBlack, 150)
     g.strokeWeight(1)
 
-    transitions.foreach { trans => drawTrans(trans, time) }
+    models.foreach { model => drawModel(model, System.currentTimeMillis()) }
   }
 
   def pointableDragged(pos: DoctusPoint): Unit = () // Nothing to do here
@@ -166,8 +163,8 @@ case class MorphingDoctusTemplate(canvas: DoctusCanvas) extends DoctusTemplate {
   def pointableReleased(pos: DoctusPoint): Unit = {
     val time = System.currentTimeMillis()
 
-    if (transitions.isEmpty || transitions.forall { _.terminated(time) }) {
-      createNextTrans
+    if (models.isEmpty || models.forall { _.trans.terminated(time) }) {
+      createNextModels
     }
   }
 
