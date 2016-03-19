45510f4f719e41e4946860c1beb851b38795f2fb
wwagner4 <wwagner4@gmail.com>
2016-03-19 12:58:57 +0100

    scaling works fine

--- morphing-core/src/main/scala/net/entelijan/MorphingController.scala
+++ morphing-core/src/main/scala/net/entelijan/MorphingController.scala
@@ -20,7 +20,7 @@ case class MorphingDoctusTemplate(canvas: DoctusCanvas) extends DoctusTemplate {
 
   val random = new java.util.Random
 
-  def dispLinePrep(angle: Int): DoctusVector = {
+  def dispLinePrepare(angle: Int): DoctusVector = {
     val len = 30.0
     val lenh = len / 2.0
     DoctusVector(0, lenh).rot(angle * math.Pi / 180)
@@ -30,25 +30,63 @@ case class MorphingDoctusTemplate(canvas: DoctusCanvas) extends DoctusTemplate {
     g.line(center + angleVector, center - angleVector)
   }
 
-  val disps = Stream.continually(random.nextInt(360)).map { angle => dispLinePrep(angle) }
+  val disps = Stream.continually(random.nextInt(360)).map { angle => dispLinePrepare(angle) }
 
   override val frameRate = Some(20)
 
   val pixImages = List(PixImageFactory.no0160, PixImageFactory.no0260, PixImageFactory.no0360,
     PixImageFactory.no0460, PixImageFactory.no0560, PixImageFactory.no0760, PixImageFactory.no0860)
 
-  val pointImages = pixImages.map { pix => pointImg(pix) }
+  val pointImages = pixImages.map { pix => createPointImg(pix) }
 
   var currentImg = 0
   var models = List.empty[Model]
 
+  // Between 0.0 and 1.0
+  def ran(): Double = random.nextDouble()
+
+  def createPointImg(pi: PixImage): List[DoctusPoint] = {
+
+    @tailrec
+    def createPoints(cnt: Int, points: List[DoctusPoint], img: PixImage): List[DoctusPoint] = {
+
+      def isPoint(x: Double, y: Double, img: PixImage): Boolean = {
+        val i: Int = math.floor(x * img.width).toInt
+        val j: Int = math.floor(y * img.height).toInt
+        val index = i * img.height + j
+        val bright = img.pixels(index)
+        val ranBright = ran()
+        ranBright > bright
+      }
+
+      if (cnt == 0) points
+      else {
+        val x = ran();
+        val y = ran();
+        if (isPoint(x, y, img)) {
+          val r = img.width.toDouble / img.height
+          val p =
+            if (r < 1.0) DoctusPoint((1 - r) / 2 + x * r, y)
+            else DoctusPoint(x, (r - 1) / 2 + y / r)
+          createPoints(cnt - 1, p :: points, img)
+        } else {
+          createPoints(cnt, points, img)
+        }
+      }
+    }
+
+    createPoints(5000, List.empty[DoctusPoint], pi)
+  }
+
   def createNextModels: Unit = {
     val time = System.currentTimeMillis()
+    val w = canvas.width
+    val h = canvas.height
 
     def createTransitions(img1: List[DoctusPoint], img2: List[DoctusPoint], startTime: Long): List[Trans] = {
 
       def randomOffset: DoctusVector = {
-        val n = 0
+        val n = 3
         val xIntOff = random.nextInt(2 * n + 1) - n
         val yIntOff = random.nextInt(2 * n + 1) - n
         DoctusVector(xIntOff, yIntOff)
@@ -66,49 +104,22 @@ case class MorphingDoctusTemplate(canvas: DoctusCanvas) extends DoctusTemplate {
       transisions.zip(disps).map { case (trans, disp) => Model(trans, dispLine(disp)) }
     }
 
-    val p1 = pointImages(currentImg)
-    val p2 = pointImages((currentImg + 1) % pointImages.size)
+    def scale(p: DoctusPoint): DoctusPoint = {
+      val r = w.toDouble / h
+      if (r < 1) DoctusPoint(p.x, (1 - r) / 2 + p.y * r)
+      else {
+        val r1 = 1 / r
+        DoctusPoint((1 - r1) / 2 + p.x * r1, p.y)
+      }
+    }
+
+    val p1 = pointImages(currentImg).map { point => scale(point) }
+    val p2 = pointImages((currentImg + 1) % pointImages.size).map { point => scale(point) }
     val transitions = createTransitions(p1, p2, time)
 
     models = createModels(transitions)
     currentImg = (currentImg + 1) % pixImages.size
 
-  }
-
-  // Between 0.0 and 1.0
-  def ran(): Double = random.nextDouble()
-
-  def pointImg(pi: PixImage): List[DoctusPoint] = {
-    createPoints(5000, List.empty[DoctusPoint], pi)
-  }
-
-  case class Scale(scalex: Double, scaley: Double, offx: Double, offy: Double)
-
-  @tailrec
-  private def createPoints(cnt: Int, points: List[DoctusPoint], img: PixImage): List[DoctusPoint] = {
-    if (cnt == 0) points
-    else {
-      val x = ran();
-      val y = ran();
-      if (isPoint(x, y, img)) {
-        val  r = img.width.toDouble / img.height
-        val p = 
-          if (r < 1.0) DoctusPoint((1 - r) / 2 + x * r, y)
-          else DoctusPoint(x, (r - 1) / 2 + y / r)
-        createPoints(cnt - 1, p :: points, img)
-      } else {
-        createPoints(cnt, points, img)
-      }
-    }
-  }
-
-  def isPoint(x: Double, y: Double, img: PixImage): Boolean = {
-    val i: Int = math.floor(x * img.width).toInt
-    val j: Int = math.floor(y * img.height).toInt
-    val index = i * img.height + j
-    val bright = img.pixels(index)
-    val ranBright = ran()
-    ranBright > bright
   }
 
   def draw(g: DoctusGraphics): Unit = {
@@ -146,7 +157,7 @@ case class MorphingDoctusTemplate(canvas: DoctusCanvas) extends DoctusTemplate {
     //g.fill(DoctusColorBlack, 10)
     //g.noStroke()
     g.stroke(DoctusColorBlack, 255)
-    g.strokeWeight(0.1)
+    g.strokeWeight(1)
 
     models.foreach { model => drawModel(model, System.currentTimeMillis()) }
   }
